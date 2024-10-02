484a458e7c8652549f7a96365dbe4d3510fe2c31
Afobaboa <gushind1984@gmail.com>
2024-10-02 13:07:24 +0300

    Canary test doesn't work...

--- sources/canaryTest.cpp
+++ sources/canaryTest.cpp
@@ -4,6 +4,8 @@
 #include "../headers/canary.h"
 #include "../headers/stack.h"
 
+#include <stdio.h>
+
 
 //----------------------------------------------------------------------------------------
 
@@ -11,7 +13,42 @@
 /**
  * 
  */
-static void StackSpoileLeftCanary(Stack* stack);
+#define CHECK_PUSH_POP_RETURNED_ERROR(stack, expectedStackError)      \
+    CheckPushPopReturnedError(GET_PLACE(), stack, expectedStackError)
+
+
+//----------------------------------------------------------------------------------------
+
+
+/** 
+ * 
+ */
+static bool StackLeftCanaryTest();
+
+
+/**
+ * 
+ */
+static bool StackRightCanaryTest();
+
+
+/**
+ * 
+ */
+static bool DataLeftCanaryTest();
+
+
+/**
+ * 
+ */
+static bool DataRightCanaryTest();
+
+
+/**
+ * 
+ */
+static bool CheckPushPopReturnedError(Place place,
+                                      Stack* stack, stackError_t expectedStackError);
 
 
 //----------------------------------------------------------------------------------------
@@ -22,28 +59,18 @@ int main()
     #ifndef CANARY_SWITCH_OFF
     LOG_OPEN();
 
-    Stack* stack = NULL;
-    STACK_CREATE(stack, sizeof(int));
-    StackSpoileLeftCanary(stack);
-    // int* dataLeftCanary = *((int**) ((char*) stack + sizeof(canary_t)));
-    // *dataLeftCanary = 666;
+    if (StackLeftCanaryTest()  &&
+        StackRightCanaryTest() &&
+        DataLeftCanaryTest()   /*&&
+        DataRightCanaryTest()*/)
+    {
+        ColoredPrintf(GREEN, "Canary test is complete.\n");
+    }
+    else 
+    {
+        ColoredPrintf(RED, "Canary test isn't complete.\n");
+    }
 
-    int          elem       = 666;
-    stackError_t stackError = OK;
-
-    stackError = StackPush(stack, &elem);
-    if (stackError != OK)
-        ColoredPrintf(RED, "Push(): stackError = %s\n", StackGetErrorName(stackError));
-
-    stackError = StackPop(stack, &elem);
-    
-    if (stackError != OK)
-        ColoredPrintf(RED, "Pop(): stackError = %s\n", StackGetErrorName(stackError));
-
-    ColoredPrintf(YELLOW, "test's done.\n");
-
-    STACK_DUMP(stack);
-    StackDelete(&stack);
     LOG_CLOSE();
     
     #else
@@ -57,7 +84,110 @@ int main()
 //----------------------------------------------------------------------------------------
 
 
-static void StackSpoileLeftCanary(Stack* stack)
+static bool StackLeftCanaryTest()
 {
-    *((char*) stack + 5 /*48*/) = 'c'; 
+    printf("%s\n", __FUNCTION__);
+    Stack* stack = NULL;
+    STACK_CREATE(stack, 1);
+
+    *((char*) stack) = 'f';
+
+    if (CHECK_PUSH_POP_RETURNED_ERROR(stack, STACK_CANARY_LEFT_SPOILED))
+    {
+        StackDelete(&stack);
+        return false;
+    }
+
+    StackDelete(&stack);
+    return true;
+}
+
+
+static bool StackRightCanaryTest()
+{
+    printf("%s\n", __FUNCTION__);
+    Stack* stack = NULL;
+    STACK_CREATE(stack, 1);
+
+    *((char*) stack + sizeof(canary_t) + sizeof(void*) + 3 * sizeof(size_t) 
+                                                     ON_DEBUG( + sizeof(void*))) = 'f';
+    
+    if (CHECK_PUSH_POP_RETURNED_ERROR(stack, STACK_CANARY_LEFT_SPOILED))
+    {
+        StackDelete(&stack);
+        return false;
+    }
+
+    StackDelete(&stack);
+    return true;
+}
+
+
+static bool DataLeftCanaryTest()
+{  
+    Stack* stack = NULL;
+    STACK_CREATE(stack, 1);
+
+    char* dataBuffer = *((char**) ((char*) stack + sizeof(canary_t)));
+    *dataBuffer = 'f';
+
+    if (CHECK_PUSH_POP_RETURNED_ERROR(stack, DATA_CANARY_LEFT_SPOILED))
+    {
+        StackDelete(&stack);
+        return false;
+    }
+
+    StackDelete(&stack);
+    return true;
+}
+
+
+static bool DataRightCanaryTest()
+{
+    Stack* stack = NULL;
+    STACK_CREATE(stack, 1);
+
+    char* dataBuffer = *((char**) ((char*) stack + sizeof(canary_t)));
+    *(dataBuffer + sizeof(canary_t)) = 'f';
+
+    if (CHECK_PUSH_POP_RETURNED_ERROR(stack, DATA_CANARY_RIGHT_SPOILED))
+    {
+        StackDelete(&stack);
+        return false;
+    }
+
+    StackDelete(&stack);
+    return true;
+}
+
+
+static bool CheckPushPopReturnedError(Place place, 
+                                      Stack* stack, stackError_t expectedStackError)
+{
+    char         elem       = 'f';
+    stackError_t stackError = OK;
+
+    stackError = StackPush(stack, &elem);
+    if (stackError != expectedStackError)
+    {
+        LogPrint(ERROR, place, "Push(): stackError = %s\n"
+                               "\texpectedStackError = %s\n", 
+                                StackGetErrorName(stackError), 
+                                StackGetErrorName(expectedStackError));
+        printf("push\n");
+        return false;
+    }
+
+    stackError = StackPop(stack, &elem);
+    if (stackError != expectedStackError)
+    {
+        LogPrint(ERROR, place,  "Pop():  stackError = %s\n"
+                                "\texpectedStackError = %s\n", 
+                                StackGetErrorName(stackError),
+                                StackGetErrorName(expectedStackError));
+        printf("pop\n");
+        return false;
+    }
+
+    return true;
 }
