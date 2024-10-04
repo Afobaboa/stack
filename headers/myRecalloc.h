2ff96addf944152caba25ff6455ccdad80c175be
Afobaboa <gushind1984@gmail.com>
2024-10-04 18:58:51 +0300

    myRalloc.h documentation is added

--- headers/myRecalloc.h
+++ headers/myRecalloc.h
@@ -12,10 +12,19 @@
 
 
 /**
+ * This is analogue of realloc() with calloc() args style.
+ * New data buffer has new address, this function can't use old address.
+ * This function free oldData. Other features is like in realloc().
  * 
+ * @param oldData         Pointer to old buffer.
+ * @param oldDataCapacity Capacity of old buffer.
+ * @param newDataCapacity Capacity of new buffer.
+ * @param elemSize        Size of each elem in bytes.
+ * 
+ * @return Ptr to new data buffer or NULL if reallocation failed or if new data size is 0.
  */
-void* MyRecalloc(void* oldData, const size_t oldDataSize,
-                 const size_t newElemCount, const size_t elemSize);
+void* MyRecalloc(void* oldData, const size_t oldDataCapacity,
+                 const size_t newDataCapacity, const size_t elemSize);
 
 
 //----------------------------------------------------------------------------------------
